426bcb395e6cffd31dd911d1f4f2aa7df7c56a1b
MM AASHIR AWAN <112618842+Aashir1234@users.noreply.github.com>
2025-03-13 20:12:49 +0500

    Update all.h

--- include/all.h
+++ include/all.h
@@ -1,5 +1,4 @@
 #ifndef ADD_H
-#define ADD_H
 struct sample{
   int sample1;
   int sample2;
@@ -9,20 +8,17 @@ int add(int a, int b);
 #endif // ADD_H
 
 #ifndef DIVISION_H
-#define DIVISION_H
 
 int divide(int a, int b);
 
 #endif // DIVISION_H
 
 #ifndef MULTIPLICATION_H
-#define MULTIPLICATION_H
 
 int multiply(int a, int b);
 
 #endif // MULTIPLICATION_H
 #ifndef SUBTRACT_H
-#define SUBTRACT_H
 
 int subtract(int a, int b);
 
